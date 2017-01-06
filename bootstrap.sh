347a01acb2a35bea66631591324311ee94c989c7
Adam Engstrom <adaengst@gmail.com>
2017-01-06 11:33:08 +0100

    Add tigrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,6 +14,7 @@ ln -s $PWD/gdbinit.simple ~/.gdbinit
 ln -s $PWD/vimperatorrc ~/.vimperatorrc
 ln -s $PWD/ssh_config ~/.ssh/config
 ln -s $PWD/gitconfig ~/.gitconfig
+ln -s $PWD/tigrc ~/.tigrc
 mkdir -p ~/.config/mc
 ln -s $PWD/mc ~/.config/mc/ini
 ln -s prelude ~/.emacs.d
