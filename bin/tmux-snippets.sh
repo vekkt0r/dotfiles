dd48cf83762b1e27e4e19392fb4a0b9c68b657f8
Adam Engstrom <adaengst@gmail.com>
2021-02-27 11:56:12 +0100

    tmux-snippets: fzf instead of peco

--- bin/tmux-snippets.sh
+++ bin/tmux-snippets.sh
@@ -5,7 +5,7 @@ then
     TMUX_WINDOW=$(tmux list-window | awk '/(active)/{print $(NF-1)}')
     TMPFILE=$(mktemp /tmp/.tmux-snippets.XXXX)
 
-    tmux new-window -n "snippets" "grep -Ev '^(#|$)' $HOME/.snippets | peco > $TMPFILE;$0 $TMUX_WINDOW $TMPFILE"
+    tmux new-window -n "snippets" "grep -Ev '^(#|$)' $HOME/.snippets | fzf > $TMPFILE;$0 $TMUX_WINDOW $TMPFILE"
 else
     tmux send-key -t $1 "$(cat $2)"
     rm -f $2
