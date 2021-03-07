2a0b5b0b64e0a873b2653e7ec25c4f6bb4354fd5
Adam Engstrom <adaengst@gmail.com>
2021-03-07 12:44:24 +0100

    hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -12,4 +12,21 @@ require("alfred")
 -- Turn off Animations.
 --
 hs.window.animationDuration = 0
+--hs.loadSpoon("Display")
+function caffeinateWatcher(eventType)
+    if (eventType == hs.caffeinate.watcher.systemWillSleep or
+            eventType == hs.caffeinate.watcher.screensDidSleep) then
+            print ("WillSleep...")
+            -- Execute sleep script
+            hs.task.new("/Users/adam/.displaysleep", nil):start()
+    elseif (eventType == hs.caffeinate.watcher.screensDidWake) then
+            print ("Woken...")
+            -- Execute wake script
+            hs.task.new("/Users/adam/.displaywakeup", nil):start()
+    end
+end
+
+sleepWatcher = hs.caffeinate.watcher.new(caffeinateWatcher)
+sleepWatcher:start()
+print ("Starting display.lua")
 
