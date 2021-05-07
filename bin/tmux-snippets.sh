189bf92460ecd502acd3b70483a834c98f3a02a7
Adam Engstrom <adaengst@gmail.com>
2021-05-07 20:41:20 +0200

    tmux-snippets: use display-popup if available

--- bin/tmux-snippets.sh
+++ bin/tmux-snippets.sh
@@ -5,7 +5,11 @@ then
     TMUX_WINDOW=$(tmux list-window | awk '/(active)/{print $(NF-1)}')
     TMPFILE=$(mktemp /tmp/.tmux-snippets.XXXX)
 
-    tmux new-window -n "snippets" "grep -Ev '^(#|$)' $HOME/.snippets | fzf > $TMPFILE;$0 $TMUX_WINDOW $TMPFILE"
+    if [[ -n $(tmux list-commands popup) ]]; then
+      tmux popup -E -w 100% "grep -Ev '^(#|$)' $HOME/.snippets | fzf > $TMPFILE;$0 $TMUX_WINDOW $TMPFILE"
+    else
+      tmux new-window -n "snippets" "grep -Ev '^(#|$)' $HOME/.snippets | fzf > $TMPFILE;$0 $TMUX_WINDOW $TMPFILE"
+    fi
 else
     tmux send-key -t $1 "$(cat $2)"
     rm -f $2
