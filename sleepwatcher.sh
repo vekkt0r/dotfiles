6f1dd675d5efaedc7012d2e259948abde0c38a65
Adam Engstrom <adaengst@gmail.com>
2022-08-23 21:24:14 +0200

    sleepwatcher: New IP

--- sleepwatcher.sh
+++ sleepwatcher.sh
@@ -5,12 +5,12 @@ CEC_CLIENT=/usr/local/bin/cec-client
 case `basename $0` in
   .displaysleep)
     echo "off $(date)" >> /tmp/display_on.txt
-    curl http://192.168.0.161/win\&T\=0
+    curl http://10.0.0.2/win\&T\=0
     echo "standby 0" | $CEC_CLIENT -s
     ;;
   .displaywakeup)
     echo "on $(date)" >> /tmp/display_on.txt
-    curl http://192.168.0.161/win\&T\=1
+    curl http://10.0.0.2/win\&T\=1
     #echo "on 0" | $CEC_CLIENT -s ; sleep 5 ; echo "as" | $CEC_CLIENT -s
     ;;
 esac
