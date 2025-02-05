ce08596125c2ff2f6d8ff56d0a55fb0020795e89
Adam Engstrom <adaengst@gmail.com>
2025-02-05 22:56:20 +0100

    nvim: updates

--- nvchad/lua/options.lua
+++ nvchad/lua/options.lua
@@ -3,3 +3,6 @@ require "nvchad.options"
 local o = vim.o
 o.mouse = ""
 o.clipboard = ""
+
+local opt = vim.opt
+opt.whichwrap:remove { "<>[]hl" }
--- nvchad/lua/plugins/init.lua
+++ nvchad/lua/plugins/init.lua
@@ -7,6 +7,7 @@ return {
 
   {
     "tpope/vim-sleuth",
+    event = "VeryLazy",
   },
 
   {
