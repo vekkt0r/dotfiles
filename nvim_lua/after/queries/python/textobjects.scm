2d8ae07585f85ff5e4e669959e06e556ff5d5775
Adam Engstrom <adaengst@gmail.com>
2025-02-25 23:18:24 +0100

    nvim_lua: snippets

--- nvim_lua/after/queries/python/textobjects.scm
+++ nvim_lua/after/queries/python/textobjects.scm
@@ -1,12 +1,20 @@
 ; extends
 (module
- (comment) @cell.start
- (#lua-match? @cell.start "^# %%%%")
- .
- (_)+ @cell.content
- (#not-any-lua-match? @cell.content "^# %%%%")
- .
- (comment)? @cell.end
- (#lua-match? @cell.end "^# %%%%")
- (#make-range! "cell.outer" @cell.start @cell.content)
+  (comment) @cell.start
+  (#eq? @cell.start "# %%")
+  .
+  [
+    (import_statement)* @cell.content
+    (expression_statement)* @cell.content
+    (function_definition)* @cell.content
+    (class_definition)* @cell.content
+    (if_statement)* @cell.content
+    (for_statement)* @cell.content
+    (while_statement)* @cell.content
+    (comment)* @cell.content
+  ]
+  .
+  (comment)? @cell.end
+  (#eq? @cell.end "# %%")
+  (#make-range! "cell.outer" @cell.start @cell.content)
 )
--- nvim_lua/lua/plugins/completion.lua
+++ nvim_lua/lua/plugins/completion.lua
@@ -1,17 +1,18 @@
 return {
   {
     'saghen/blink.cmp',
-    -- optional: provides snippets for the snippet source
     dependencies = {
       { 'rafamadriz/friendly-snippets' },
       {
-        -- snippet plugin
         'L3MON4D3/LuaSnip',
         dependencies = 'rafamadriz/friendly-snippets',
         opts = { history = true, updateevents = 'TextChanged,TextChangedI' },
         config = function(_, opts)
-          require('luasnip.loaders.from_snipmate').lazy_load { paths = { vim.fn.stdpath 'config' .. '/snippets/lua' } }
-          require('luasnip.loaders.from_lua').lazy_load { paths = { vim.fn.stdpath 'config' .. '/snippets/snipmate' } }
+          require('luasnip').config.set_config(opts)
+          require('luasnip.loaders.from_lua').load()
+          require('luasnip.loaders.from_lua').lazy_load { paths = { vim.fn.stdpath 'config' .. '/snippets/lua' } }
+          require('luasnip.loaders.from_snipmate').load()
+          require('luasnip.loaders.from_snipmate').lazy_load { paths = { vim.fn.stdpath 'config' .. '/snippets/snipmate' } }
         end,
       },
     },
@@ -22,6 +23,7 @@ return {
     ---@type blink.cmp.Config
     ---@diagnostic disable-next-line: missing-fields
     opts = {
+      snippets = { preset = 'luasnip' },
       keymap = {
         preset = 'super-tab',
       },
