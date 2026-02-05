51e1c8ab22196f5bfab6d65770375cf1e0ed8d0d
Adam Engstrom <adaengst@gmail.com>
2026-02-05 20:36:20 +0100

    nvim.lua: gitsigns navigation

--- nvim_lua/lua/plugins/gitsigns.lua
+++ nvim_lua/lua/plugins/gitsigns.lua
@@ -6,5 +6,35 @@ return {
       delete = { text = '󰍵' },
       changedelete = { text = '󱕖' },
     },
+    on_attach = function(bufnr)
+      local gitsigns = require 'gitsigns'
+      local function map(mode, lhs, rhs, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, lhs, rhs, opts)
+      end
+
+      -- next hunk
+      map('n', ']c', function()
+        if vim.wo.diff then
+          return ']c'
+        end
+        vim.schedule(function()
+          gitsigns.next_hunk()
+        end)
+        return '<Ignore>'
+      end, { expr = true })
+
+      -- previous hunk
+      map('n', '[c', function()
+        if vim.wo.diff then
+          return '[c'
+        end
+        vim.schedule(function()
+          gitsigns.prev_hunk()
+        end)
+        return '<Ignore>'
+      end, { expr = true })
+    end,
   },
 }
