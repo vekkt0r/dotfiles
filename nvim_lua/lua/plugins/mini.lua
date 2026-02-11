fbe5efc04e269fae1434ed292add82e599e142e0
Adam Engstrom <adaengst@gmail.com>
2026-02-11 22:08:28 +0100

    nvim_lua: statusbar color on modified

--- nvim_lua/lua/plugins/mini.lua
+++ nvim_lua/lua/plugins/mini.lua
@@ -3,7 +3,37 @@ return {
   version = false,
   config = function()
     local statusline = require 'mini.statusline'
-    statusline.setup { use_icons = true }
+    statusline.setup {
+      use_icons = true,
+      content = {
+        active = function()
+          local mode, mode_hl = statusline.section_mode { trunc_width = 120 }
+          local git = statusline.section_git { trunc_width = 75 }
+          local diagnostics = statusline.section_diagnostics { trunc_width = 75 }
+          local filename = statusline.section_filename { trunc_width = 140 }
+          local fileinfo = statusline.section_fileinfo { trunc_width = 120 }
+          local location = statusline.section_location { trunc_width = 75 }
+          local search = statusline.section_searchcount { trunc_width = 75 }
+
+          local actual_mode_hl = vim.bo.modified and 'MiniStatuslineModeModified' or mode_hl
+
+          return statusline.combine_groups {
+            { hl = actual_mode_hl, strings = { mode } },
+            { hl = 'MiniStatuslineDevinfo', strings = { git, diagnostics } },
+            { hl = 'MiniStatuslineFilename', strings = { filename } },
+            { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+            { hl = 'MiniStatuslineLocation', strings = { location } },
+            { hl = 'MiniStatuslineSearch', strings = { search } },
+          }
+        end,
+      },
+    }
+
+    vim.api.nvim_set_hl(0, 'MiniStatuslineModeModified', {
+      fg = '#000000',
+      bg = '#ffcc00',
+      bold = true,
+    })
 
     -- require('mini.completion').setup()
     require('mini.comment').setup()
