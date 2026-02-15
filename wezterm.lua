b25e20d1b14308584c30bdb510e864b949ea236d
Adam Engstrom <adaengst@gmail.com>
2026-02-15 18:23:19 +0100

    wezterm: font

--- wezterm.lua
+++ wezterm.lua
@@ -2,7 +2,8 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.color_scheme = "Solarized (dark) (terminal.sexy)"
-config.font = wezterm.font("Hack Nerd Font Mono")
+-- config.font = wezterm.font("Hack Nerd Font Mono", { weight = "Regular" })
+config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "DemiBold" })
 config.hide_tab_bar_if_only_one_tab = true
 config.window_close_confirmation = "NeverPrompt"
 config.key_tables = {
