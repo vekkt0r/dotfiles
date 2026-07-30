dafce9d47c442877fef0dde02fcce6b7e0a21028
Adam Engstrom <adaengst@gmail.com>
2026-07-30 14:18:02 +0200

    fish: fzf setup for interactive

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
 
+    set -qU FZF_TMUX_HEIGHT ; or set -Ux FZF_TMUX_HEIGHT '100%'
+    fzf --fish | source
+
     bind alt-backspace backward-kill-token
 end
